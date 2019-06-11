e22fe294fbfcabb14ba45e1366e6e806034f0ba0
alexlezama90 <alexlezama90@hotmail.com>
2019-06-10 22:31:02 -0500

    homework section 7 finished

--- hello-world/src/app/course/course.component.ts
+++ hello-world/src/app/course/course.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-course',
+  selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  courseCategories = [
+    {id:1, name:"Development"},
+    {id:2, name:"Art"},
+    {id:3, name:"Languages"},
+
+  ]
 
   constructor() { }
 
